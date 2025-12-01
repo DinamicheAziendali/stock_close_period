de7098cba9970a82c396ca26164bdb1fc94310d5
Andrea Barbato <abarbato@dinamicheaziendali.it>
2025-12-01 12:56:48 +0100

    [FIX]stock_close_period: aggiunto controllo in compute stock_quant

--- stock_close_period/models/product_product.py
+++ stock_close_period/models/product_product.py
@@ -54,7 +54,7 @@ class Product(models.Model):
         )
 
         for row2 in self._cr.fetchall():
-            quant_qty = row2[0] * 1.0
+            quant_qty = float(row2[0] or 0.0) * 1.0
             product_id = row2[1]
             location_id = row2[2]
             lot_id = (
